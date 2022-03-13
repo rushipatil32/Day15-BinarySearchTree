e38f02e8e8c4105b15aa4f6bd245663bea9b19b7
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-13 15:49:32 +0530

    Added:ability to create bst as shown in figure

--- src/com/blz/binarysearchtree/BinarySearchTree.java
+++ src/com/blz/binarysearchtree/BinarySearchTree.java
@@ -2,12 +2,13 @@ package com.blz.binarysearchtree;
 
 public class BinarySearchTree {
 
-	    Node root;
+	 Node root;
 	    public static void main(String[] args) {
-	        BinarySearchTree bst = new BinarySearchTree();
-	        bst.insertion(56);
-	        bst.insertion(30);
-	        bst.insertion(70);
+	    	BinarySearchTree bst = new BinarySearchTree();
+	        int[] nodeData = {56, 30, 70, 22, 40, 11, 3, 16, 60, 95, 65, 63, 67};
+	        for (int data : nodeData) {
+	            bst.insertion(data);
+	        }
 	        bst.displayBST(bst.root);
 	    }
 
@@ -17,20 +18,20 @@ public class BinarySearchTree {
 	        n.left = null;
 	        n.right = null;
 
-	        if(root == null){   //If tree is empty
+	        if(root == null){
 	            root = n;
 	        } else{
 	            Node parent = root;
 	            Node current = root;
-	            while(current != null){  //Finding Parent Of Node n.
+	            while(current != null){
 	                parent = current;
-	                if(n.data > current.data){
+	                if(n.data>current.data){
 	                    current = current.right;
 	                }else{
 	                    current = current.left;
 	                }
 	            }
-	            if(n.data > parent.data){
+	            if(n.data> parent.data){
 	                parent.right = n;
 	            }else{
 	                parent.left = n;
@@ -43,7 +44,6 @@ public class BinarySearchTree {
 	            displayBST(node.left);
 	        }
 	        System.out.println(node.data);
-
 	        if (node.right != null) {
 	            displayBST(node.right);
 	        }

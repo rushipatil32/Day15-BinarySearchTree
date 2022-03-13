6efebd5d31d0799862e9472cc8bead6015599367
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-13 15:54:52 +0530

    Added:Ability to search in BST

--- src/com/blz/binarysearchtree/BinarySearchTree.java
+++ src/com/blz/binarysearchtree/BinarySearchTree.java
@@ -1,15 +1,15 @@
 package com.blz.binarysearchtree;
 
 public class BinarySearchTree {
-
 	 Node root;
 	    public static void main(String[] args) {
-	    	BinarySearchTree bst = new BinarySearchTree();
+	    	BinarySearchTree  bst = new BinarySearchTree();
 	        int[] nodeData = {56, 30, 70, 22, 40, 11, 3, 16, 60, 95, 65, 63, 67};
 	        for (int data : nodeData) {
 	            bst.insertion(data);
 	        }
 	        bst.displayBST(bst.root);
+	        bst.search(bst.root,63);
 	    }
 
 	    public void insertion(int data){
@@ -48,4 +48,15 @@ public class BinarySearchTree {
 	            displayBST(node.right);
 	        }
 	    }
-}
+
+	    public void search(Node node, int data) {
+	        if (node.data == data)
+	            System.out.println(data + " present");
+	        else if (node.data < data && node.right != null)
+	            search(node.right, data);
+	        else if (node.data > data && node.left != null)
+	            search(node.left, data);
+	        else
+	            System.out.println(data + " not present");
+	    }
+}	    
